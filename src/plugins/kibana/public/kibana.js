36b3de3d66f86c2d8128ae5efd2458997c829609
Alvaro del Castillo <acs@bitergia.com>
2016-01-20 20:04:16 +0100

    Read metadashboards from .kibana to build tabs.

--- src/plugins/kibana/public/kibana.js
+++ src/plugins/kibana/public/kibana.js
@@ -10,8 +10,18 @@ const moment = require('moment-timezone');
 const chrome = require('ui/chrome');
 const routes = require('ui/routes');
 const modules = require('ui/modules');
+const Scanner = require('ui/utils/scanner');
+const _ = require('lodash');
 
 const kibanaLogoUrl = require('ui/images/kibana.svg');
+
+function setTabs(metadashboards) {
+  var tabs = [];
+  _.each(metadashboards, function (title, dash) {
+    tabs.push({id:title, title: dash});
+  });
+  chrome.setTabs(tabs);
+}
 
 routes.enable();
 
@@ -52,10 +62,29 @@ chrome
     title: 'Settings'
   }
 ])
-.setRootController('kibana', function ($scope, $rootScope, courier, config) {
+.setRootController('kibana', function ($scope, $rootScope, courier, config, es, kbnIndex) {
   function setDefaultTimezone() {
     moment.tz.setDefault(config.get('dateFormat:tz'));
   }
+
+  function getMetaDashboards() {
+    var queryString = '';
+    var pageSize = 1000;
+
+    var scanner = new Scanner(es, {
+      index: kbnIndex,
+      type: 'metadashboard'
+    });
+
+    return scanner.scanAndMap(queryString, {
+      pageSize,
+      docCount: Infinity
+    }, function (hit) {return hit;});
+  }
+
+  getMetaDashboards().then(function (results) {
+    setTabs(results.hits[0]._source);
+  });
 
   // wait for the application to finish loading
   $scope.$on('application.load', function () {
