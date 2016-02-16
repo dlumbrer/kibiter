6257ab676f0c1d3069d0ee3cc46bcc1c0e1489a5
Alvaro del Castillo <acs@bitergia.com>
2016-02-16 06:06:44 +0100

    [Vis data] Added an artifical bucket with others information so it is show in all vis types.

--- src/ui/public/vislib_vis_type/buildChartData.js
+++ src/ui/public/vislib_vis_type/buildChartData.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import AggResponseIndexProvider from 'ui/agg_response/index';
 import AggResponseTabifyTableProvider from 'ui/agg_response/tabify/_table';
 
@@ -7,6 +8,17 @@ export default function VislibVisBuildChartData(Private) {
 
   return function (esResponse) {
     var vis = this.vis;
+
+    if (esResponse.aggregations) {
+      var aggs = esResponse.aggregations[_.keys(esResponse.aggregations)[0]];
+      if (aggs.sum_other_doc_count) {
+        // Add another bucket with sum_other_doc_count
+        aggs.buckets.push({
+          'key':'Others',
+          'doc_count': aggs.sum_other_doc_count
+        });
+      }
+    }
 
     if (vis.isHierarchical()) {
       // the hierarchical converter is very self-contained (woot!)
