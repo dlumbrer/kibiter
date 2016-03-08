6c8c0a4c69c939eebe0afd2057db4caea95730dc
Alvaro del Castillo <acs@bitergia.com>
2016-03-08 19:23:46 +0100

    [Docker image] Complete node binaries download first to avoid problems.

--- tasks/build/index.js
+++ tasks/build/index.js
@@ -7,6 +7,7 @@ module.exports = function (grunt) {
       'clean:build',
       'clean:target',
       '_build:downloadNodeBuilds:start',
+      '_build:downloadNodeBuilds:finish',
       'copy:devSource',
       'babel:build',
       '_build:babelOptions',
@@ -20,7 +21,7 @@ module.exports = function (grunt) {
       'clean:deepModules',
       'run:optimizeBuild',
       'stop:optimizeBuild',
-      '_build:downloadNodeBuilds:finish',
+      // '_build:downloadNodeBuilds:finish',
       '_build:versionedLinks',
       '_build:archives',
       grunt.option('os-packages') ? [
