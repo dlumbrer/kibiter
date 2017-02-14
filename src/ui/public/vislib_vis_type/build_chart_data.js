df468f9528aa4764e1ee07e0219776dd7ca230d4
Alvaro del Castillo <acs@bitergia.com>
2017-02-14 23:12:18 +0100

    [others] Support unique count aggregations for Others result

--- src/ui/public/vislib_vis_type/build_chart_data.js
+++ src/ui/public/vislib_vis_type/build_chart_data.js
@@ -10,14 +10,22 @@ export default function VislibVisBuildChartData(Private) {
 
     if (esResponse.aggregations) {
       var aggs = esResponse.aggregations[_.keys(esResponse.aggregations)[0]];
-      if (aggs.sum_other_doc_count) {
-       // Add another bucket with sum_other_doc_count
-       aggs.buckets.push({
-         'key':'Others',
-         'doc_count': aggs.sum_other_doc_count
-       });
+      if (aggs.buckets[0][1]) {
+        // Unique count
+        aggs.buckets.push({
+          '1': {"value": aggs.sum_other_doc_count},
+          'key':'Others',
+          'doc_count': aggs.sum_other_doc_count
+        });
       }
-     }
+      else {
+        // Add another bucket with sum_other_doc_count
+        aggs.buckets.push({
+          'key':'Others',
+          'doc_count': aggs.sum_other_doc_count
+        });
+      }
+    }
 
     if (vis.isHierarchical()) {
       // the hierarchical converter is very self-contained (woot!)
