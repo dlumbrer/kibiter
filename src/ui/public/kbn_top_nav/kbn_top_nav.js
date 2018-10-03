24cd41840c570104400a719083758dfcd00ff4a9
David Moreno <dmorenolumb@gmail.com>
2018-10-03 16:05:28 +0200

    Change css when a dashboard is selected

--- src/ui/public/kbn_top_nav/kbn_top_nav.js
+++ src/ui/public/kbn_top_nav/kbn_top_nav.js
@@ -205,7 +205,29 @@ module.directive('kbnTopNav', function (es, kbnIndex, Private) {
         }
       }).then(function (resp) {
         scope.$root.metadash = resp.hits.hits[0]._source.metadashboard;
+        //Put the selected one in different color
+        styleSelected(scope, scope.$root.metadash)
       })
+
+      const styleSelected = (scope, metadash) => {
+        scope.$root.selectedItem = undefined
+        let currentDash = window.location.href.split("#/dashboard/")[1].split('?')[0]
+        metadash.some((item) => {
+          if(item.type === "entry" && item.link === currentDash){
+            scope.$root.selectedItem = item
+          }else if (item.type === "menu"){
+            item.dashboards.some((subitem) => {
+              if(subitem.type === "entry" && subitem.link === currentDash){
+                scope.$root.selectedItem = item
+                return true
+              }
+            })
+          }
+          if (scope.$root.selectedItem){
+            return true
+          }
+        });
+      }
 
       scope.$root.appTitleCustom = "GrimoireLab"
       es.search({
