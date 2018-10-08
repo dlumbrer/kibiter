476b13b3cd4832cd652eb82eaa910da7fcd3eb9c
David Moreno <dmorenolumb@gmail.com>
2018-10-08 15:56:27 +0200

    Reformat code adding an array in order to show the columns

--- src/ui/public/kbn_top_nav/custom_menu.js
+++ src/ui/public/kbn_top_nav/custom_menu.js
@@ -14,25 +14,20 @@ export function customMenu($scope) {
             $scope.showNewMenu = true;
             $scope.parentDashboard = item;
             // Divide in 4 columns
+            let numberColumns = 4
+            $scope.currentPanelsons = new Array(numberColumns)
             let countItems = 0;
-            $scope.currentPanelsons_fourth = []
-            $scope.currentPanelsons_third = []
-            $scope.currentPanelsons_second = []
-            $scope.currentPanelsons_first = []
             item.dashboards.forEach(function (subitem) {
-                if (countItems === 0) {
-                    $scope.currentPanelsons_first.push(subitem)
-                } else if (countItems === 1) {
-                    $scope.currentPanelsons_second.push(subitem)
-                } else if (countItems === 2) {
-                    $scope.currentPanelsons_third.push(subitem)
-                } else {
-                    $scope.currentPanelsons_fourth.push(subitem)
+                if (!$scope.currentPanelsons[countItems]){
+                    $scope.currentPanelsons[countItems] = []
+                }
+                $scope.currentPanelsons[countItems].push(subitem)
+                if (countItems >= numberColumns-1){
                     countItems = 0;
                     return
                 }
                 countItems++
-            });
+            })
         }
     }
 
